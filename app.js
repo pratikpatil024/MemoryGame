4d910e96c7c1998eb87a21eb6bd96269b9f58a3f
pratikpatil024 <pratikspatil024@gmail.com>
2020-04-28 20:20:43 +0530

    done till creatBoard

---
+++ app.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // card options
+  const cardArray = [
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+  ]
+
+  const grid = document.querySelector('.grid')
+
+  // create board
+  function createBoard(){
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img')
+      card.setAttribute('src', 'image/blank.png')
+      card.setAttribute('data-id', i)
+      // card.addEventListener('click', flipcard)
+      grid.appendChild(card)
+    }
+  }
+
+  createBoard()
+
+
+})
